966a9913dff75daf55763a28f67e88d3b0b952db
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-27 16:07:01 +0200

    create table and seeders

--- database/migrations/2023_06_27_131735_create_trains_table.php
+++ database/migrations/2023_06_27_131735_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station',30);
+            $table->string('arrival_station', 30);
+            $table->datetime('departure_time');
+            $table->datetime('arrival-time');
+            $table->char('train_code', 8);
+            $table->tinyinteger('number_of_carriages',);
+            $table->boolean('on_time');
+            $table->boolean('deleted');
             $table->timestamps();
         });
     }
